35a50630483a527e562e10c47241d198f347db3a
Pda <anh22032005@gmail.com>
2025-07-15 05:59:40 +0700

    thêm chức năng call, lấy API trực tiếp products, giỏ hàng, hoàn thiên detail, tìm kiếm sản phẩm, thanh toán

---
+++ app/src/main/java/com/example/myapplication/Cart.java
@@ -0,0 +1,9 @@
+// Cart.java
+package com.example.myapplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    public static List<Product> cartItems = new ArrayList<>();
+}
